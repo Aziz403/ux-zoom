4c6e2ad841ca9d1fe79ebc71ecea9375cfb9636c
aziz403 <azizbenmallouk4@gmail.com>
2022-12-16 12:09:25 +0100

    init bundle

---
+++ src/ZoomBundle.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Aziz403 <azizbenmallouk4@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Aziz403\UX\Zoom;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author Aziz403 <azizbenmallouk4@gmail.com>
+ *
+ * @final
+ */
+class ZoomBundle extends Bundle
+{
+
+}
